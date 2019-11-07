bfa3619ad9d322d9878d4af9bbb38d76eb3af8db
AlissonAlmeidaSS <alisson.ifpb@gmail.com>
2019-11-07 09:35:00 -0300

    Alter class

--- src/main/java/app.java
+++ src/main/java/app.java
@@ -12,26 +12,26 @@ public class app {
             a = reader.read("POLYGON((10 10, 20 50, 40 10, 10 10))");
             b = reader.read("POLYGON((40 10, 60 10, 60 50, 20 50, 40 10))");
 
-        System.out.println(metodosGeometry.containsAB(a,b));
-        System.out.println(metodosGeometry.containsBA(a,b));
-        System.out.println(metodosGeometry.coversAB(a,b));
-        System.out.println(metodosGeometry.coversBA(a,b));
-        System.out.println(metodosGeometry.overlapsAB(a,b));
-        System.out.println(metodosGeometry.overlapsBA(a,b));
-        System.out.println(metodosGeometry.touchesAB(a,b));
-        System.out.println(metodosGeometry.touchesBA(a,b));
-        System.out.println(metodosGeometry.withinAB(a,b));
-        System.out.println(metodosGeometry.withinBA(a,b));
-        System.out.println(metodosGeometry.equalsAB(a,b));
-        System.out.println(metodosGeometry.equalsBA(a,b));
-        System.out.println(metodosGeometry.disjointAB(a,b));
-        System.out.println(metodosGeometry.disjointBA(a,b));
-        System.out.println(metodosGeometry.intersectsAB(a,b));
-        System.out.println(metodosGeometry.intersectsBA(a,b));
-        System.out.println(metodosGeometry.crossesAB(a,b));
-        System.out.println(metodosGeometry.crossesBA(a,b));
-        System.out.println(metodosGeometry.coveredByAB(a,b));
-        System.out.println(metodosGeometry.coveredByBA(a,b));
+        System.out.println("Contains AB = " + metodosGeometry.containsAB(a,b));
+        System.out.println("Contains BA = " + metodosGeometry.containsBA(a,b));
+        System.out.println("Covers AB = " + metodosGeometry.coversAB(a,b));
+        System.out.println("Covers BA = " + metodosGeometry.coversBA(a,b));
+        System.out.println("Overlaps AB = " + metodosGeometry.overlapsAB(a,b));
+        System.out.println("Overlaps BA = " + metodosGeometry.overlapsBA(a,b));
+        System.out.println("Touches AB = " + metodosGeometry.touchesAB(a,b));
+        System.out.println("Touches BA = " + metodosGeometry.touchesBA(a,b));
+        System.out.println("Winthin AB = " + metodosGeometry.withinAB(a,b));
+        System.out.println("Winthin BA = " + metodosGeometry.withinBA(a,b));
+        System.out.println("Equals AB = " + metodosGeometry.equalsAB(a,b));
+        System.out.println("Equals BA = " + metodosGeometry.equalsBA(a,b));
+        System.out.println("Disjoint AB = " + metodosGeometry.disjointAB(a,b));
+        System.out.println("Disjoint BA = " + metodosGeometry.disjointBA(a,b));
+        System.out.println("Intersects AB = " + metodosGeometry.intersectsAB(a,b));
+        System.out.println("Intersects BA = " + metodosGeometry.intersectsBA(a,b));
+        System.out.println("Crosses AB = " + metodosGeometry.crossesAB(a,b));
+        System.out.println("Crosses BA = " + metodosGeometry.crossesBA(a,b));
+        System.out.println("CoveredBy AB = " + metodosGeometry.coveredByAB(a,b));
+        System.out.println("CoveredBy BA = " + metodosGeometry.coveredByBA(a,b));
 
 
     }
